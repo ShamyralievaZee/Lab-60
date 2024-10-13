85479651e9381f0e98b26f4ba4ed26a168872c9c
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-10-13 16:10:45 +0600

    Worked on sendMessage Post request, and handleSubmit. Made sure it posts the message successfully and clears out the input & textarea once message is posted/sent away.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { useState} from "react";
+import React, { useState} from "react";
 
 const App = () => {
   const [author, setAuthor] = useState("");
   const [message, setMessage] = useState("");
 
+  const sendMessage = async () => {
+    const data = new URLSearchParams();
+    data.set("author", author);
+    data.set("message", message);
+
+    await fetch("http://146.185.154.90:8000/messages", {
+      method: "POST",
+      body: data,
+    });
+
+    setAuthor("");
+    setMessage("");
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await sendMessage();
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3 w-10 m-4">
           <label htmlFor="author" className="form-label">Author:</label>
           <input
